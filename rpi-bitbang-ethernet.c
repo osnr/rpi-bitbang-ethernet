2c575725abda81ebe195727bd8b7a70ec4abd4d8
Omar Rizwan <omar.rizwan@gmail.com>
2020-09-11 20:42:35 -0700

    Blink works!

--- rpi-bitbang-ethernet.c
+++ rpi-bitbang-ethernet.c
@@ -8,8 +8,24 @@ const unsigned int source_ip[] = {192, 168, 1, 44};
 const unsigned int dest_ip[] = {192, 168, 1, 6};
 const unsigned int dest_phys[] = {0x78, 0x4F, 0x43, 0x88, 0x3B, 0xE2};
 
+void sleep(int n) {
+    // 0x3F0000 = about 3 seconds?
+    for (volatile int i = 0; i < n; i++) {}
+}
+
 void main(void) {
     unsigned char frame[256];
+
+    __asm__(
+            "ldr r0,=0xFE200000   \n"
+            "mov r1, #1           \n"
+            "lsl r1,#6            \n"  /* -> 001 000 000 */
+            "str r1,[r0,#0x10]    \n"
+            "mov r1,#1            \n"
+            "lsl r1,#10           \n"
+
+            "str r1,[r0,#0x2C]  \n"
+            : : : "r0","r1" );
 
     for (;;) {
         unsigned int ip_checksum = 
@@ -18,29 +34,24 @@ void main(void) {
         ip_checksum = (ip_checksum & 0x0000FFFF) + (ip_checksum >> 16);
         ip_checksum = (ip_checksum & 0x0000FFFF) + (ip_checksum >> 16);
 
-        // FIXME: sleep 1s
-        
         __asm__(
             "ldr r0,=0xFE200000   \n"
-            "mov r1, #1           \n"
-            "lsl r1,#6            \n"  /* -> 001 000 000 */
-            "str r1,[r0,#0x10]    \n"
             "mov r1,#1            \n"
             "lsl r1,#10           \n"
-            "loop$:               \n"
-              "str r1,[r0,#0x2C]  \n"
-              "mov r2,#0x3F0000   \n"
-              "wait1$:            \n"
-                "sub r2,#1        \n"
-                "cmp r2,#0        \n"
-                "bne wait1$       \n"
-              "str r1,[r0,#0x20]  \n"
-              "mov r2,#0x3F0000   \n"
-              "wait2$:            \n"
-                "sub r2,#1        \n"
-                "cmp r2,#0        \n"
-                "bne wait2$       \n"
-              "b loop$            \n"
-        );
+
+            "str r1,[r0,#0x2C]  \n"
+            : : : "r0","r1" );
+        
+        sleep(0x3F0000);
+
+        __asm__(
+            "ldr r0,=0xFE200000   \n"
+            "mov r1,#1            \n"
+            "lsl r1,#10           \n"
+
+            "str r1,[r0,#0x20]  \n"
+            : : : "r0","r1" );
+
+        sleep(0x3F0000);
     }
 }
