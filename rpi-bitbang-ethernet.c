df93bdfa47d55a1d39e2b6aebc3d4610cfa65178
Omar Rizwan <omar.rizwan@gmail.com>
2020-09-19 23:26:40 -0700

    enable MMU. 10 microseconds! omg

--- rpi-bitbang-ethernet.c
+++ rpi-bitbang-ethernet.c
@@ -115,8 +115,52 @@ void uart_puts(char* s) {
     for (int i = 0; i < 2; i++) uart_putc(s[i]);
 }
 
+void EnableMMU (void)
+{
+  static volatile __attribute__ ((aligned (0x4000))) unsigned PageTable[4096];
+
+  unsigned base;
+  for (base = 0; base < 512; base++)
+  {
+    // outer and inner write back, write allocate, shareable
+    PageTable[base] = base << 20 | 0x1140E;
+  }
+  for (; base < 4096; base++)
+  {
+    // shared device, never execute
+    PageTable[base] = base << 20 | 0x10416;
+  }
+
+  // restrict cache size to 16K (no page coloring)
+  unsigned auxctrl;
+  __asm__ volatile ("mrc p15, 0, %0, c1, c0,  1" : "=r" (auxctrl));
+  auxctrl |= 1 << 6;
+  __asm__ volatile ("mcr p15, 0, %0, c1, c0,  1" :: "r" (auxctrl));
+
+  // set domain 0 to client
+  __asm__ volatile ("mcr p15, 0, %0, c3, c0, 0" :: "r" (1));
+
+  // always use TTBR0
+  __asm__ volatile ("mcr p15, 0, %0, c2, c0, 2" :: "r" (0));
+
+  // set TTBR0 (page table walk inner cacheable, outer non-cacheable, shareable memory)
+  __asm__ volatile ("mcr p15, 0, %0, c2, c0, 0" :: "r" (3 | (unsigned) &PageTable));
+
+  // invalidate data cache and flush prefetch buffer
+  /* __asm__ volatile ("mcr p15, 0, %0, c7, c5,  4" :: "r" (0) : "memory"); */
+  /* __asm__ volatile ("mcr p15, 0, %0, c7, c6,  0" :: "r" (0) : "memory"); */
+
+  // enable MMU, L1 cache and instruction cache, L2 cache, write buffer,
+  //   branch prediction and extended page table on
+  unsigned mode;
+  __asm__ volatile ("mrc p15,0,%0,c1,c0,0" : "=r" (mode));
+  mode |= 0x0480180D;
+  __asm__ volatile ("mcr p15,0,%0,c1,c0,0" :: "r" (mode) : "memory");
+}
+
 void main(void) {
-    /* gpio_set_as_alt4(24); gpio_set_as_alt4(25); gpio_set_as_alt4(26); gpio_set_as_alt4(27); */
+    EnableMMU();
+
     const unsigned char source_ip[] = {192, 168, 1, 44};
 
     // destination set to my laptop's MAC and IP address; you should change this for yours.
